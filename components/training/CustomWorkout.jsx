c9f000ddfee0e88a64873a25144367ab63fa5e8d
Matt <director@acidgambit.com>
2023-02-24 21:11:18 -0600

    add comments in custom workout

--- components/training/CustomWorkout.jsx
+++ components/training/CustomWorkout.jsx
@@ -1,7 +1,8 @@
-import { Formik, Field, Form, useField } from "formik";
+import { Formik, Field, Form, useField, FieldArray } from "formik";
 import * as yup from "yup";
 
-const DaisyInput = ({label, ...props}) => {
+// todo: add validation for sets, reps, weight
+const DaisyInput = ({placeholder, label, ...props}) => {
   const [field, meta] = useField(props);
   return (
     <div className="form-control">
@@ -10,10 +11,11 @@ const DaisyInput = ({label, ...props}) => {
       </label>
       <input
         {...field}
-        className="input-bordered input" 
+        className="input-bordered input-sm input" 
+        placeholder={placeholder}
       />
       {meta.touched && meta.error ? (
-        <div className="pt-1 text-sm text-red">{meta.error}</div> 
+        <div className="pt-1 text-xs text-red">{meta.error}</div> 
         ) : (null)
       }
     </div>
@@ -25,12 +27,14 @@ const CustomWorkout = () => {
     <div>
       <Formik
         validateOnChange={true}
-        initialValues={{ name: "", secondName: "" }}
+        initialValues={{ 
+          workoutType: "", 
+          setsStrength: [{ movement: "", sets: "", reps: "", weight: "", id: 1 }],
+          endurance: "",
+          conditioning: ""
+        }}
         validationSchema={yup.object({
-          name: yup.string()
-            .required("Required")
-            .max(20, "Must be 20 characters or less"),
-          secondName: yup.string()
+          workoutType: yup.string()
             .required("Required")
             .max(20, "Must be 20 characters or less"),
         })}
@@ -44,11 +48,36 @@ const CustomWorkout = () => {
       >
         {({ values, handleChange, handleBlur, handleSubmit }) => (
           <Form onSubmit={handleSubmit} className="flex flex-col items-center justify-center mt-6">
-            <div className="w-72 form-control">
-              <DaisyInput name="name" value={values.name} label="Name" />
-              <DaisyInput name="secondName" value={values.seconName} label="Second Name" />
+            <div className="w-32 form-control">
+              <DaisyInput name="workoutType" value={values.workoutType} label="Workout Type" placeholder="Hybrid" />
+              <DaisyInput name="endurance" value={values.endurance} label="Endurance" placeholder="45 minute easy run" />
+              <DaisyInput name="conditioning" value={values.conditioning} label="Conditioning" placeholder="21-15-9 Burpees + Cal Row" />
+              <FieldArray name="strength">
+                {arrayHelpers => (
+                  <div>
+                    <button 
+                      onClick={() => arrayHelpers.push({
+                        movement: 'bench',
+                        sets: '3',
+                        reps: '5',
+                        weight: '135',
+                        id: "" + Math.random()
+                      })} 
+                    className="btn">add movement</button>
+                    {values.setsStrength.map((set, index) => {
+                      const name = `setsStrength.${index}.movement`;
+                      return (
+                        <div key={set.id}>
+                          <DaisyInput name={name} placeholder="bench press" label="Strength Movement" />
+                          <button onClick={() => arrayHelpers.remove(index)} className="btn">X</button>
+                        </div>
+                      )
+                    })}
+                  </div>
+                )}
+              </FieldArray>
               <div className="py-2">
-                <button type="submit" className="btn btn-accent">Submit</button>
+                <button type="submit" className="btn">Submit</button>
               </div>
               <pre>
                 {JSON.stringify(values, null, 2)}
--- pages/add.js
+++ pages/add.js
@@ -3,7 +3,7 @@ import CustomWorkout from "../components/training/CustomWorkout";
 const Add = () => {
   return (
     <div className="container text-center">
-      <h1 className="my-5 text-xl font-bold">Custom Workout</h1>
+      <h1 className="mt-5 text-xl font-bold">Add a Custom Workout</h1>
       <CustomWorkout />
     </div>
   );
